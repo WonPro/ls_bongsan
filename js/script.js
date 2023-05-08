5ca4c9d4836b2548bfe7fddc3482a47ee67382fa
JunHee Won <wonjunhee.com@gmail.com>
2023-05-08 17:53:16 +0900

    html, css 작업 완료

--- js/script.js
+++ js/script.js
@@ -31,6 +31,40 @@
 
 $(document).ready(function(){
 
+  $('#slide').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slideToShow: 1,
+    arrows: false,
+    dots: true,
+  });
+
+  $('#faqList .activeBtn').on('click', function(){
+    let active = $(this).hasClass('active');
+    let answer = $(this).parent().next('.answer');
+
+    if(!active){
+      $(this).addClass('active');
+      answer.addClass('active');
+      answer.slideDown(300);
+    } else {
+      $(this).removeClass('active');
+      answer.removeClass('active');
+      answer.slideUp(300);
+    }
+  });
+
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: "#instragram",
+      start: "center center",
+      end: "bottom top",
+      scrub: true,
+      pin: true
+    }
+  })
+    .from("#info",  { y: 5 });
+
   
   // AOS start
   // AOS.init();
