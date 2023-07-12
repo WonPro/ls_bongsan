fd8ca60fb12ce20de8b0416408af7e36ba2994d8
JunHee Won <wonjunhee.com@gmail.com>
2023-07-12 17:57:09 +0900

    팝업추가

--- js/script.js
+++ js/script.js
@@ -5,23 +5,23 @@
 *********************************************************/
 
   // Open Modal Event
-  // function openPopup(popupName) {
-  //   let modalWrap = document.getElementById('popupWrap');
-  //   let popup = document.getElementById('popup' + popupName);
+  function openPopup(popupName) {
+    let modalWrap = document.getElementById('popupWrap');
+    let popup = document.getElementById('popup' + popupName);
 
-  //   modalWrap.classList.add('on');
-  //   popup.classList.add('on');
-  // }
+    modalWrap.classList.add('on');
+    popup.classList.add('on');
+  }
 
 
   // Close Modal Event
-  // function closePopup(popupName) {
-  //   let modalWrap = document.getElementById('popupWrap');
-  //   let popup = document.getElementById('popup' + popupName);
+  function closePopup(popupName) {
+    let modalWrap = document.getElementById('popupWrap');
+    let popup = document.getElementById('popup' + popupName);
 
-  //   popup.classList.remove('on');
-  //   modalWrap.classList.remove('on');
-  // }
+    popup.classList.remove('on');
+    modalWrap.classList.remove('on');
+  }
 
 
 
